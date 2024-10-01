4bbbe15fc41658059859cda93ab3395c307d7007
Nacho <isanccal@myuax.com>
2024-10-01 14:26:07 +0200

    cambio

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -11,18 +11,18 @@ class TestCalculator(unittest.TestCase):
         self.calc.resta(2, 2)
         self.assertEqual(self.calc.value, 0)
     def test_multiplicacion(self):
-        self.calc.multiplicacion(2, 2)
-        self.assertEqual(self.calc.value, 4)
+        self.calc.multiplicacion(9, 9)
+        self.assertEqual(self.calc.value, 81)
     def test_division(self):
-        self.calc.division(2, 2)
+        self.calc.division(7, 7)
         self.assertEqual(self.calc.value, 1)
     def test_potencia(self):
-        self.calc.potencia(2,2)
-        self.assertEqual(self.calc.value, 4)
+        self.calc.potencia(8,8)
+        self.assertEqual(self.calc.value, 64)
     def test_raiz(self):
-        self.calc.raiz(4)
-        self.assertEqual(self.calc.value, 2)
+        self.calc.raiz(100)
+        self.assertEqual(self.calc.value, 10)
     def test_factorial(self):
-        self.calc.factorial(4)
-        self.assertEqual(self.calc.value, 24)
+        self.calc.factorial(5)
+        self.assertEqual(self.calc.value, 120)
     
