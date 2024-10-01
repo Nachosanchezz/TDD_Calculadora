3c0ec21140c4fbf5da959c2fa1913ed2f967504e
Nacho <isanccal@myuax.com>
2024-10-01 12:42:37 +0200

    aa

--- src/calculadora.py
+++ src/calculadora.py
@@ -6,32 +6,37 @@ class Calculator():
     
     def suma(self, a, b):
         self.value = a + b
-        return a + b
+        return self.value
+        
     def resta(self, a, b):
         self.value = a - b
-        return a - b
+        return self.value
+        
     def multiplicacion(self, a, b):
         self.value = a * b
-        return a * b
+        return self.value
+        
     def division(self, a, b):
         if b == 0:
             raise ValueError("No se puede dividir por cero")
         self.value = a / b
-        return a / b
+        return self.value
+        
     def potencia (self, a, b):
         self.value = a**b
-        return a**b
+        return self.value
+       
     def raiz(self, a):
         if a < 0:
             raise ValueError("No se puede sacar raiz cuadrada de un numero negativo")
         self.value = math.sqrt(a)
         return self.value
-    def factorial(self, numero):
-        if numero < 0:
-            raise ValueError("El factorial no está definido para números negativos")
-        elif numero == 0:
-            return 1
-        factor = 1
-        for i in range(1, numero + 1):
-            factor *= i
-        return factor
+    def factorial(self, a):
+        if a == 0:
+            self.value = 1
+        else:
+            self.value = 1
+            for i in range(1, a + 1):
+                self.value *= i
+        return self.value
+        
